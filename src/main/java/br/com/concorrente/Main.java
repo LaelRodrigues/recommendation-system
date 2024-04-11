8213da10e9d8d8ed42bd942864db3b7ad7f36001
LaelRodrigues <laelrodrigues7@gmail.com>
2024-04-11 12:52:44 -0300

    versão atualização da solução com thread de plataforma usando 4 threads

--- src/main/java/br/com/concorrente/Main.java
+++ src/main/java/br/com/concorrente/Main.java
@@ -9,39 +9,37 @@ public class Main {
 
         String caminhoArquivo = "src/main/resources/teste2.csv";
 
-        // Crie um objeto File com o caminho do arquivo
         File file = new File(caminhoArquivo);
 
-        // Verifique se o arquivo existe e é um arquivo válido
         if (file.exists() && file.isFile()) {
-            // Obtenha o tamanho do arquivo em bytes
+
             long tamanhoArquivo = file.length();
 
-            // Dividindo o arquivo em 3 partes
-            int parte = (int) tamanhoArquivo / 3;
+            int parte = (int) tamanhoArquivo / 4;
 
-            // Criando threads para ler cada parte do arquivo
-            DataManager dataManager1 = new DataManager(caminhoArquivo, 0, parte);
-            DataManager dataManager2 = new DataManager(caminhoArquivo, parte, parte * 2);
-            DataManager dataManager3 = new DataManager(caminhoArquivo, parte * 2, (int) tamanhoArquivo);
+            DataManager part1 = new DataManager(caminhoArquivo, 0, parte);
+            DataManager part2 = new DataManager(caminhoArquivo, parte, parte * 2);
+            DataManager part3 = new DataManager(caminhoArquivo, parte * 2, parte * 2);
+            DataManager part4 = new DataManager(caminhoArquivo, parte * 3, (int) tamanhoArquivo);
+
 
             long startTime = System.currentTimeMillis();
 
-            Thread thread1 = Thread.ofPlatform().start(dataManager1);
-            Thread thread2 = Thread.ofPlatform().start(dataManager2);
-            Thread thread3 = Thread.ofPlatform().start(dataManager3);
+            Thread thread1 = Thread.ofPlatform().start(part1);
+            Thread thread2 = Thread.ofPlatform().start(part2);
+            Thread thread3 = Thread.ofPlatform().start(part3);
+            Thread thread4 = Thread.ofPlatform().start(part4);
+
 
             // Esperando que todas as threads terminem
             try {
                 thread1.join();
                 thread2.join();
                 thread3.join();
+                thread4.join();
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
-
-            // Agora você pode usar os dados carregados
-            System.out.println("tamanho da matriz: " + dataManager1.getRatingsMatrix().size());
 
             long endTime = System.currentTimeMillis();
 
@@ -49,22 +47,24 @@ public class Main {
 
             System.out.println("tempo de resposta: " + duration + " em milissegundos");
 
-            DataManager dataManager = new DataManager();
+            DataManager combinedData = new DataManager();
 
-            dataManager.mergeData(dataManager1);
-            dataManager.mergeData(dataManager2);
-            dataManager.mergeData(dataManager3);
+            combinedData.mergeData(part1);
+            combinedData.mergeData(part2);
+            combinedData.mergeData(part3);
+            combinedData.mergeData(part4);
 
             startTime = System.currentTimeMillis();
 
-            Thread thread11 = Thread.ofPlatform().start(() -> processRecommendations(dataManager, "A2F6NONFUDB6UK"));
-            Thread thread22 = Thread.ofPlatform().start(() -> processRecommendations(dataManager, "A25MD5I2GUIW6W"));
-            Thread thread33 = Thread.ofPlatform().start(() -> processRecommendations(dataManager, "A373VVEU6Z9M0N"));
-
+            thread1 = Thread.ofPlatform().start(() -> processRecommendations(combinedData, "A2F6NONFUDB6UK"));
+            thread2 = Thread.ofPlatform().start(() -> processRecommendations(combinedData, "A25MD5I2GUIW6W"));
+            thread3 = Thread.ofPlatform().start(() -> processRecommendations(combinedData, "A373VVEU6Z9M0N"));
+            thread4 = Thread.ofPlatform().start(() -> processRecommendations(combinedData, "caminhoArquivo"));
+            
             try {
-                thread11.join();
-                thread22.join();
-                thread33.join();
+                thread1.join();
+                thread2.join();
+                thread3.join();
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
@@ -78,6 +78,7 @@ public class Main {
             System.out.println("O arquivo especificado não existe ou não é válido.");
         }
     }
+
     private static void processRecommendations(DataManager dataManager, String userId) {
         RecommenderService recommenderService = new RecommenderService(dataManager);
         int k = 5;
